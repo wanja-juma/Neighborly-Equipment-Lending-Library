e69125fc1c1f830a574e3d260c9f512c48cbd6c6
henry-m111 <henry.muchiri@student.moringaschool.com>
2026-08-20 01:29:07 +0300

    Add Footer component and wire it into App

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -1,8 +1,14 @@
 import Home from './components/Home';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
-  return <Home />;
+  return (
+    <>
+      <Home />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
