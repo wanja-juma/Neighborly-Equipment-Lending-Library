46bd5a22f89c50409c8977f3926d3807266c5f04
PlayfulAJ <jabaringure@gmail.com>
2026-08-20 00:07:07 +0300

    Navbar Implementation and basic css

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -1,8 +1,14 @@
 import Home from './components/Home';
+import Navbar from './components/Navbar';
 import './App.css';
 
 function App() {
-  return <Home />;
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
 }
 
 export default App;
