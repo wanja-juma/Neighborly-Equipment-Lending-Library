cec71738326a05e50ed9f7b7848e7cd47ef1e68e
ruthjuma100@gmail.com <ruthjuma100@gmail.com>
2026-08-21 22:02:12 +0300

    update app.jsx with routes

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -1,32 +1,47 @@
-import AuthPage from './components/AuthPage.jsx';
-import Dashboard from './components/Dashboard';
-import HowItWorks from './components/HowItWorks';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import { Navigate, Route, Routes } from "react-router-dom";
+import AuthPage from "./components/AuthPage";
+import Dashboard from "./components/Dashboard";
+import DashboardLayout from "./components/DashboardLayout";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import BrowseItems from "./pages/BrowseItems";
+import DamageReports from "./pages/DamageReports";
+import Loans from "./pages/Loans";
+import MyListings from "./pages/MyListings";
+import Requests from "./pages/Requests";
+import "./App.css";
 
-import './App.css';
-
-
-const App = () => {
+function App() {
   return (
-  
-    <div>    
     <>
-
-      <HowItWorks />
       <Navbar />
-      <AuthPage />
-      <Dashboard /> 
-      <Footer />
-  
 
+      <Routes>
+        <Route path="/" element={<Home />} />
 
+        <Route path="/auth" element={<AuthPage />} />
+
+        <Route element={<DashboardLayout />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/items" element={<BrowseItems />} />
+          <Route path="/listings" element={<MyListings />} />
+          <Route path="/requests" element={<Requests />} />
+          <Route path="/loans" element={<Loans />} />
+
+          <Route
+            path="/damage-reports"
+            element={<DamageReports />}
+          />
+        </Route>
+
+        <Route
+          path="*"
+          element={<Navigate to="/" replace />}
+        />
+      </Routes>
     </>
-      </div>
-  
-  
   );
 }
 
-export default App
+export default App;
 
