83805d546280e1babbeab564d5132775d1a9031f
ruthjuma100@gmail.com <ruthjuma100@gmail.com>
2026-08-21 19:00:17 +0300

    Resolve merge conflicts in App.jsx and HowItWorks.jsx

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -1,22 +1,31 @@
 import AuthPage from './components/AuthPage.jsx';
+import Dashboard from './components/Dashboard';
+import Home from './components/Home';
 import HowItWorks from './components/HowItWorks';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
 import './App.css';
 
-import Dashboard from './components/Dashboard'
 
 const App = () => {
   return (
-    <div>
+  
+    <div>    
+    <>
+      <Home />
+      <HowItWorks />
       <Navbar />
       <HowItWorks />
       <AuthPage />
       <Dashboard /> 
       <Footer />
-    </div>
+  
 
+
+    </>
+      </div>
+  
   
   );
 }
