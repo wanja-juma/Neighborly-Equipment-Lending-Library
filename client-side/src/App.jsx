37521a4ad65dbd3f2a539046addc552075862adb
henry-m111 <henry.muchiri@student.moringaschool.com>
2026-08-22 17:59:24 +0300

    Wire up routes for BrowseTools and ItemDetail.

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -1,17 +1,21 @@
+import { Routes, Route } from 'react-router-dom';
 import AuthPage from './components/AuthPage.jsx';
 import HowItWorks from './components/HowItWorks';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import PaymentBar from './components/PaymentBar';
+import BrowseTools from './components/BrowseTools';
+import ItemDetail from './components/ItemDetail';
 import './App.css';
 
 function App() {
   return (
     <>
-      <HowItWorks />
       <Navbar />
-      <AuthPage />
-      <PaymentBar itemName="Cordless Drill" dailyRate={5} />
+      <Routes>
+        <Route path="/" element={<><HowItWorks /><AuthPage /></>} />
+        <Route path="/tools" element={<BrowseTools />} />
+        <Route path="/tools/:id" element={<ItemDetail />} />
+      </Routes>
       <Footer />
     </>
   );
