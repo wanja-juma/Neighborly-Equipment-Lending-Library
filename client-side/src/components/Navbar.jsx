e79206d1ec4047d2bef72d49b993c7e2f672eab1
PlayfulAJ <jabaringure@gmail.com>
2026-08-25 19:09:13 +0300

    fix home navigation scrolling to go back up after clicking the about navigation

--- client-side/src/components/Navbar.jsx
+++ client-side/src/components/Navbar.jsx
@@ -49,10 +49,14 @@ function Navbar() {
       </Link>
 
       <div className="navbar__links">
-        <Link to="/">
-          <House size={18} />
-          Home
-        </Link>
+
+      <Link // this makes the home navbar link scroll to the top of the page when clicked (made hand in hand with the about feature).
+        to="/"
+        onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+      >
+        <House size={18} />
+        Home
+      </Link>
 
         <Link to="/#about">
           <Info size={18} />
