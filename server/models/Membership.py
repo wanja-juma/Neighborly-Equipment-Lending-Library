82c3af2e2c229104e52575f0fa02bf4f68699f1a
Mark Kamau <mark.wainaina@student.moringaschool.com>
2026-08-26 10:41:51 +0300

    Adjusting file structure

--- server/models/Membership.py
+++ server/models/Membership.py
@@ -21,17 +21,3 @@ class Membership(db.Model):
 VALID_STATUSES = ('held', 'refunded', 'forfeited')
  
  
-class Deposit(db.Model, SerializerMixin):
-    __tablename__ = 'deposits'
- 
- 
-    id = db.Column(db.Integer, primary_key=True)
-    loan_id = db.Column(db.Integer, db.ForeignKey('loans.id'), nullable=False, unique=True)
-    amount = db.Column(db.Numeric(10, 2), nullable=False)
-    status = db.Column(db.String, nullable=False, default='held')
-    paid_at = db.Column(db.DateTime)
-    refunded_at = db.Column(db.DateTime) 
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, server_default=db.func.now(), onupdate=db.func.now())   
-
-    loan = db.relationship('Loan', back_populates='deposit')
