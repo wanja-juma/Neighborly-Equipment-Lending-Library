529a757baa92c6ecf9d1aea9da14937b789597b3
PlayfulAJ <jabaringure@gmail.com>
2026-08-27 16:37:35 +0300

    add category model

--- server/models/categories.py
+++ server/models/categories.py
@@ -4,4 +4,19 @@ from app.extensions import db
 
 
 class Category(db.Model):
-    pass
+    __tablename__ = "categories"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(255))
+    created_at = db.Column(
+        db.DateTime,
+        default=lambda: datetime.now(timezone.utc)
+    )
+    updated_at = db.Column(
+        db.DateTime,
+        default=lambda: datetime.now(timezone.utc),
+        onupdate=lambda: datetime.now(timezone.utc)
+    )
+
+    items = db.relationship("Item", backref="category")
