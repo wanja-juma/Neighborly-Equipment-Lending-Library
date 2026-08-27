c715ca893c0d8b855570e4cd83e50d6a82837c62
henry-m111 <henry.muchiri@student.moringaschool.com>
2026-08-27 08:04:20 +0300

    Add created_at/updated_at timestamps to User model

--- server/models.py
+++ server/models.py
@@ -22,3 +22,5 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     password_hash = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
