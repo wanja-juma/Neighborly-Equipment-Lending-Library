1faf7cfa677a44c386da8547634b538169ceaa11
henry-m111 <henry.muchiri@student.moringaschool.com>
2026-08-27 08:04:20 +0300

    Add BorrowingRequest model with core fields

--- server/models.py
+++ server/models.py
@@ -24,3 +24,9 @@ class User(db.Model):
     password_hash = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+
+class BorrowingRequest(db.Model):
+    __tablename__ = "borrowing_requests"
+
+    id = db.Column(db.Integer, primary_key=True)
