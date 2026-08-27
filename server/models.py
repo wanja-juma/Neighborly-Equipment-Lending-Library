44460236df5b764bd507718bda23e8f619ae0290
henry-m111 <henry.muchiri@student.moringaschool.com>
2026-08-27 08:04:20 +0300

    Add status and notification fields to BorrowingRequest

--- server/models.py
+++ server/models.py
@@ -32,3 +32,6 @@ class BorrowingRequest(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     item_id = db.Column(db.Integer, db.ForeignKey("items.id", ondelete="CASCADE"), nullable=False)
     borrower_id = db.Column(db.Integer, db.ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+
+    status = db.Column(db.String(20), nullable=False, default="pending")
+    notification = db.Column(db.String(255), nullable=True)
