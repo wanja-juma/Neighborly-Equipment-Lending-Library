e7a886a657bf87006c11ad723b0ad63764b9e7eb
henry-m111 <henry.muchiri@student.moringaschool.com>
2026-08-27 08:04:21 +0300

    Configure SQLALCHEMY_DATABASE_URI for local Postgres

--- server/app.py
+++ server/app.py
@@ -3,5 +3,8 @@ from extensions import db
 
 def create_app():
     app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://henry:yourpassword@localhost/neighborly"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
     db.init_app(app)
     return app
