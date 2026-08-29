c60973911ea4831a5d972c3ea699dca607d19bff
rhun-akulloh <rhunakulloh@gmail.com>
2026-08-29 17:24:52 +0300

    refactor: add comment to the update items route

--- server/routes/item.py
+++ server/routes/item.py
@@ -94,6 +94,7 @@ class ItemDetail(Resource):
         return {"item": _serialize(item)}
 
     def patch(self, item_id):
+        #updates an item in the system.
         item = Item.query.get(item_id)
         if item is None:
             return {"message": "Item not found"}, 404
