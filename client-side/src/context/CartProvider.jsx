0d93b77d51b6c5b884e85d229961cec63f168c56
henry-m111 <henry.muchiri@student.moringaschool.com>
2026-09-02 22:30:21 +0300

    add addToCart and removeFromCart functions

--- client-side/src/context/CartProvider.jsx
+++ client-side/src/context/CartProvider.jsx
@@ -3,6 +3,19 @@ import CartContext from "./CartContext";
 
 function CartProvider({ children }) {
   const [cartItems, setCartItems] = useState([]);
+
+  function addToCart(item) {
+    setCartItems((current) => {
+      if (current.some((i) => i.id === item.id)) {
+        return current;
+      }
+      return [...current, item];
+    });
+  }
+
+  function removeFromCart(itemId) {
+    setCartItems((current) => current.filter((i) => i.id !== itemId));
+  }
 }
 
 export default CartProvider;
